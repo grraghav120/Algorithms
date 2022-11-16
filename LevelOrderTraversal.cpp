7387fb9ebf304a28b7f483e2d1312f2d6cd948cc
RAGHAV GARG <96789493+grraghav120@users.noreply.github.com>
2022-11-16 19:27:58 +0530

    Update LevelOrderTraversal.cpp

--- LevelOrderTraversal.cpp
+++ LevelOrderTraversal.cpp
@@ -1,1 +1,26 @@
 
+// Contributed by Raghav Garg
+
+#include <bits/stdc++.h>
+using namepace std;
+
+vector<vector<int>> LevelOrderTraversal(Treenode* root){
+    vector<vector<int>> traverse;
+    if(root){
+        queue<Treenode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> levels;
+            while(n--){
+                Treenode * node=q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                levels.push_back(node->val);
+            }
+            traverse.push_back(levels);
+        }
+    }
+    return traverse;
+}
