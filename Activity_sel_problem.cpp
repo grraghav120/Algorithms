8f032bd038776f647c2b8e6c1fbfda890c5a1d79
RAGHAV GARG <96789493+grraghav120@users.noreply.github.com>
2022-11-20 15:42:09 +0530

    Update Activity_sel_problem.cpp

--- Activity_sel_problem.cpp
+++ Activity_sel_problem.cpp
@@ -18,3 +18,39 @@ int main()
     for(auto it: ans) cout<<(it)<<" ";
     return 0;
 }
+
+// When already sorted arrays
+
+// but when not sorted complexity will be O(nlogn)
+
+// Algo:-
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i;cin>>n;
+    vector<int> s(n),f(n);
+    vector< pair<int,int> > ans;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> mini;
+    for(int i=0;i<n;i++) cin>>s[i];
+    for(int i=0;i<n;i++) cin>>f[i];
+    for(int i=0;i<n;i++){
+        mini.push({f[i],s[i]});
+    }
+    auto it=mini.top();
+    ans.push_back({it.second,it.first});
+    mini.pop();
+    while(!mini.empty()){
+        auto itr=mini.top();
+        if(itr.second>=it.first){
+            it=itr;
+            ans.push_back({itr.second,itr.first});
+        }
+        mini.pop();
+    }
+    for(auto it: ans) cout<<(it.first)<<" "<<it.second<<" "<<'\n';
+    return 0;
+}
