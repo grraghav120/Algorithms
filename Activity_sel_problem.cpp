f4a8465c23b2f3c7917ea856994f45a55b8f1c70
RAGHAV GARG <96789493+grraghav120@users.noreply.github.com>
2022-11-20 15:23:56 +0530

    Update Activity_sel_problem.cpp

--- Activity_sel_problem.cpp
+++ Activity_sel_problem.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-    int n;cin>>n;
+    int n,i;cin>>n;
     vector<int> s(n),f(n),ans;
-    for(int i=1;i<=n;i++) cin>>s[i];
-    for(int i=1;i<=n;i++) cin>>f[i];
-    ans.push_back(1);
-    int i=1;
-    for(int j=2;j<=n;j++){
+    for(int i=0;i<n;i++) cin>>s[i];
+    for(int i=0;i<n;i++) cin>>f[i];
+    i=0;
+    ans.push_back(0);
+    for(int j=1;j<n;j++){
         if(s[j]>=f[i]){
             ans.push_back(j);
             i=j;
         }
     }
-    for(auto it: ans) cout<<it<<" ";
+    for(auto it: ans) cout<<(it)<<" ";
     return 0;
 }
