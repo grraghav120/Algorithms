f1d38f42f2465807fe57592240174a0f44458d09
RAGHAV GARG <96789493+grraghav120@users.noreply.github.com>
2023-01-01 12:34:32 +0530

    storeGraphInCpp

---
+++ How_to_store_graph_in_c++.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int vertices,edges;
+    cin>>vertices>>edges;
+    vector<int> adj[vertices];
+    while(edges--){
+        int v1,v2;
+        cin>>v1>>v2;
+        adj[v1].push_back(v2);
+        adj[v2].push_back(v1);
+    }
+    cout<<"Printing Adjacency list of Undirected Graph "<<'\n';
+    for(int i=0;i<vertices;i++){
+        cout<<"vertex "<<i<<" -> ";
+        for(auto it:adj[i]) cout<<it<<" ";
+        cout<<'\n';
+    }
+    return 0;
+}
