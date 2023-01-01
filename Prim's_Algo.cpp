1669b583a71baf7252d71a0b1852b5c7729fc133
Pratyush Kumar <prtyush0903@gmail.com>
2023-01-01 23:39:13 +0530

    Updated Prim's_Algo.cpp
    I have not changed the given template and updated it successfully

--- Prim's_Algo.cpp
+++ Prim's_Algo.cpp
@@ -8,13 +8,37 @@ void Prims_Algo(int vertices,int edges,int min_weight,int k,int l){
     t[0][2]=l;
     vector<int> near(vertices,0);
     for(int i=0;i<vertices;i++){
+        near[i]=i;
     }
-    
-    // Print Adjacency List
-    // for(int i=0;i<vertices;i++){
-    //     for(auto it:graph[i]) cout<<it.first<<" "<<it.second<<" ";
-    //     cout<<'\n';
-    // }
+    near[l]=k;
+    int near_weight[vertices];
+    memset(near_weight,INT_MAX,sizeof(near_weight));
+    near_weight[k]=0;
+    for(int i=0;i<vertices;i++){
+        int min_v=INT_MAX,min_index;
+        for(int j=0;j<vertices;j++){
+            if(near[j]!=-1 && near_weight[j]<min_v){
+                min_v=near_weight[j];
+                min_index=j;
+            }
+        }
+        if(min_index!=l){
+            t[i][1]=near[min_index];
+            t[i][2]=min_index;
+        }
+        near[min_index]=-1;
+        for(int j=0;j<vertices;j++){
+            if(near[j]!=-1 && graph[min_index][j].second<near_weight[j]){
+                near[j]=min_index;
+                near_weight[j]=graph[min_index][j].second;
+            }
+        }
+    }
+    // Print the minimum spanning tree
+    cout<<"Minimum Spanning Tree: "<<endl;
+    for(int i=0;i<vertices-1;i++){
+        cout<<t[i][1]<<" "<<t[i][2]<<endl;
+    }
 }
 
 int main()
