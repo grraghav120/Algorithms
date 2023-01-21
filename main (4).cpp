fbf7251c5d692e8a0f538b5a5b5b6f1db8b77e74
RAGHAV GARG <96789493+grraghav120@users.noreply.github.com>
2023-01-21 20:00:04 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n,vector<vector<int>> &a){
+    long long int rowp=1,colp=1,i;
+    long long int mid=n/2;
+    for( i=0;i<n;i++){
+        rowp*=a[mid][i];
+    }
+    // cout<<rowp<<'\n';
+    for( i=0;i<n;i++){
+        colp*=a[i][mid];
+    }
+    // cout<<colp<<'\n';
+    long long int sum=rowp+colp;
+    rowp=0;
+    for(i=sum;i>0;i/=10){
+        int mod=i%10;
+        rowp=rowp*10+mod;
+    }
+    return rowp;
+}
+
+int main()
+{
+    int n;cin>>n;
+    vector<vector<int>> a(n,vector<int>(n,0));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    cout<<func(n,a)<<'\n';
+    return 0;
+}
